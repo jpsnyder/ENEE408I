eef0d4cb4291f55304a090d0d43b705448d38240
jpsnyder <jonsnyder1@gmail.com>
2014-09-09 11:21:32 -0400

    successfully imported opencv library!

--- Robo/app/src/main/java/edu/umd/enee408i/robo/MyActivity.java
+++ Robo/app/src/main/java/edu/umd/enee408i/robo/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.opencv.core.Mat;  // Finally!!!!
 
 
 public class MyActivity extends Activity {
