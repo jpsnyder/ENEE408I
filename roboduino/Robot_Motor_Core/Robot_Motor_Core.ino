28f4c88d6cd2c27e0d76d3f4d89a8074341e4e00
jpsnyder <jonsnyder1@gmail.com>
2014-09-10 13:53:58 -0400

    eshan's changes

--- roboduino/Robot_Motor_Core/Robot_Motor_Core.ino
+++ roboduino/Robot_Motor_Core/Robot_Motor_Core.ino
@@ -8,34 +8,34 @@
 */
 
 //#include <ArduinoRobotMotorBoard.h>
-int 1INA = 8;
-int 1INB = 9;
-int 1PWM = 10;
-int 2INA = 11;
-int 2INB = 12;
-int 2PWM = 13;
+int INA1 = 8;
+int INB1 = 9;
+int PWM1 = 10;
+int INA2 = 11;
+int INB2 = 12;
+int PWM2 = 13;
 
-int 1PWM_val = 127; //(25% = 64; 50% = 127; 75% = 191; 100% = 255)
-int 2PWM = 127; //(25% = 64; 50% = 127; 75% = 191; 100% = 255)
+int PWM1_val = 127; //(25% = 64; 50% = 127; 75% = 191; 100% = 255)
+int PWM2_val = 127; //(25% = 64; 50% = 127; 75% = 191; 100% = 255)
 
 
 void setup(){
-  pinMode(1INA, OUTPUT);
-  pinMode(1INB, OUTPUT);
-  pinMode(1PWM, OUTPUT);
-  pinMode(2INA, OUTPUT);
-  pinMode(2INB, OUTPUT);
-  pinMode(2PWM, OUTPUT);
+  pinMode(INA1, OUTPUT);
+  pinMode(INB1, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(INA2, OUTPUT);
+  pinMode(INB2, OUTPUT);
+  pinMode(PWM2, OUTPUT);
 }
 
 void loop(){
-  digitalWrite(1INA, HIGH);  // sets to go forward?
-  digitalWrite(1INB, LOW);
+  digitalWrite(INA1, HIGH);  // sets to go forward?
+  digitalWrite(INB1, LOW);
   
-  digitalWrite(2INA, HIGH);  // sets to go forward?
-  digitalWrite(2INB, LOW);
+  digitalWrite(INA2, HIGH);  // sets to go forward?
+  digitalWrite(INB2, LOW);
   
-  analogWrite(1PWM, 1PWM_val);
-  analogWrite(2PWM, 2PWM_val);
+  analogWrite(PWM1, PWM1_val);
+  analogWrite(PWM2, PWM2_val);
   
 }
