29dcde19efe2c16185d1c784776f0006d1c55fc3
jpsnyder <jonsnyder1@gmail.com>
2014-09-27 17:31:43 -0400

    create ArduinoController to use SerialIOManager

--- Robo/app/src/main/java/edu/umd/enee408i/robo/ArduinoController.java
+++ Robo/app/src/main/java/edu/umd/enee408i/robo/ArduinoController.java
@@ -5,20 +5,84 @@ import android.content.Context;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.HexDump;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
 import org.opencv.core;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class ArduinoController extends Activity {
+
+    TextView statusText;
+    TextView sendCommandText;  // Center text that displays command being sent to arduino
+    // TODO:  make a receive one
+    private static UsbSerialPort arduinoPort = null;  // arduino port
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private SerialInputOutputManager serialIOManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                    Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    ArduinoController.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArduinoController.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
+    private void onDeviceStateChange() {
+        stopIoManager();
+        startIoManager();
+    }
+
+    private void updateReceivedData(byte[] data) {
+        final String message = "Read " + data.length + " bytes: \n"
+                + HexDump.dumpHexString(data) + "\n\n";
+        sendCommandText.setText(message);  // display received data
+//        mDumpTextView.append(message);
+//        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+    }
+
+    private void stopIoManager() {
+        if (serialIOManager != null) {
+            Log.i(TAG, "Stopping io manager ..");
+            serialIOManager.stop();
+            serialIOManager = null;
+        }
+    }
+
+    private void startIoManager() {
+        if (arduinoPort != null) {
+            Log.i(TAG, "Starting io manager ..");
+            serialIOManager = new SerialInputOutputManager(sPort, mListener);
+            executor.submit(serialIOManager);
+        }
+    }
+
+
 
     public static byte[] stringToBytesASCII(String str) {
         char[] buffer = str.toCharArray();
@@ -29,42 +93,88 @@ public class ArduinoController extends Activity {
         return b;
     }
 
+    private final Runnable MotorController = new Runnable(){
+        public void run(){
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arduino_controller);
 
-        // Find all available drivers from attached devices
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-        if (availableDrivers.isEmpty())
-            return;
+        // setup text
+        sendCommandText = (TextView) findViewById(R.id.sendCommandText);
+        statusText = (TextView) findViewById(R.id.statusText);
+    }
 
-        // Open a connection to the first available driver.
-        UsbSerialDriver driver = availableDrivers.get(0);
-        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-        if (connection == NULL)
-            // Fudge... CHEESE IT!!!
-            return;
-
-        // TODO: create helper functions for this
-        // Write some data..
-        UsbSerialPort port = driver.getPorts().get(0);
-        try {
-            port.open(connection);
-            // set to send over 8 bits
-            port.setParameters(115200, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-            // Send command to go forward
-            port.write(stringToBytesASCII("F"), 1000);
-            Thread.sleep(5000);
-            port.write(stringToBytesASCII("B"), 1000);
-            Thread.sleep(5000);
-            port.close();
-        } catch (IOException e) {
-            // CHEESE IT!!!!
-            // TODO: display error message or something?
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (arduinoPort != null) {
+            try {
+                arduinoPort.close();
+            } catch (IOException e) {
+                // Ignore.
+            }
+            arduinoPort = null;
         }
+        finish();
+    }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        if (arduinoPort == NULL){
+            statusText.setText("Arduino Not Found!");
+        } else {
+
+            // Find all available drivers from attached devices
+            final UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+            if (availableDrivers.isEmpty()){
+                statusText.setText("No Devices Found!");
+                return;
+            }
+
+            // Open a connection to the first available driver.
+            UsbSerialDriver driver = availableDrivers.get(0);
+            UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+            if (connection == NULL) {
+                statusText.setText("Opening device Failed!");
+                return;
+            }
+
+            // TODO: create helper functions for this
+            // Write some data..
+            arduinoPort = driver.getPorts().get(0);
+            try {
+                arduinoPort.open(connection);
+                // set to send over 8 bits
+                arduinoPort.setParameters(115200, UsbSerialPort.DATABITS_8,
+                        UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+
+                // Send command to go forward then backward  for testing purposes
+                arduinoPort.write(stringToBytesASCII("F"), 1000);
+                Thread.sleep(5000);
+                arduinoPort.write(stringToBytesASCII("B"), 1000);
+                Thread.sleep(5000);
+                
+                arduinoPort.close();
+            } catch (IOException e) {
+                statusText.setText("Error opening device: " + e.getMessage());
+                try {
+                    arduinoPort.close();
+                } catch (IOException e2){
+                    // Ignore
+                }
+                return;
+            }
+            statusText.setText("Connected to: " + arduinoPort.getClass().getSimpleName());
+        }
+        onDeviceStateChange();  // Restart the IO manager
     }
 
 
