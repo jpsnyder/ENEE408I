cdedbf5967abe1ae379d3dcf118baa6601b020f0
jpsnyder <jonsnyder1@gmail.com>
2014-09-27 15:47:44 -0400

    create test command to arduino to go forward and backward

--- Robo/app/src/main/java/edu/umd/enee408i/robo/ArduinoController.java
+++ Robo/app/src/main/java/edu/umd/enee408i/robo/ArduinoController.java
@@ -1,17 +1,70 @@
 package edu.umd.enee408i.robo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import org.opencv.core;
+
+import java.io.IOException;
+import java.util.List;
+
 
 public class ArduinoController extends Activity {
+
+    public static byte[] stringToBytesASCII(String str) {
+        char[] buffer = str.toCharArray();
+        byte[] b = new byte[buffer.length];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = (byte) buffer[i];
+        }
+        return b;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arduino_controller);
+
+        // Find all available drivers from attached devices
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty())
+            return;
+
+        // Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == NULL)
+            // Fudge... CHEESE IT!!!
+            return;
+
+        // TODO: create helper functions for this
+        // Write some data..
+        UsbSerialPort port = driver.getPorts().get(0);
+        try {
+            port.open(connection);
+            // set to send over 8 bits
+            port.setParameters(115200, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            // Send command to go forward
+            port.write(stringToBytesASCII("F"), 1000);
+            Thread.sleep(5000);
+            port.write(stringToBytesASCII("B"), 1000);
+            Thread.sleep(5000);
+            port.close();
+        } catch (IOException e) {
+            // CHEESE IT!!!!
+            // TODO: display error message or something?
+        }
+
     }
 
 
