8268bc01315e91ed07c3050eb8168f1ad255fa44
jpsnyder <jonsnyder1@gmail.com>
2014-10-05 14:06:47 -0400

    move arduinoController to an activityless Class, let Main class call ArduinoController with read and write commands as it pleases

--- Robo/app/src/main/java/edu/umd/enee408i/robo/ArduinoController.java
+++ Robo/app/src/main/java/edu/umd/enee408i/robo/ArduinoController.java
@@ -1,15 +1,9 @@
 package edu.umd.enee408i.robo;
 
-import android.app.Activity;
 import android.content.Context;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
-import android.os.Bundle;
 import android.os.SystemClock;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
@@ -17,26 +11,26 @@ import com.hoho.android.usbserial.driver.UsbSerialProber;
 import com.hoho.android.usbserial.util.HexDump;
 import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
-//import org.opencv.core;
-
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
-public class ArduinoController extends Activity {
+public class ArduinoController {
 
-    TextView statusText;
-    TextView sendCommandText;  // Center text that displays command being sent to arduino
-    // TODO:  make a receive one
     private static UsbSerialPort arduinoPort = null;  // arduino port
-    private UsbManager usbManager;
+    private UsbManager usbManager = null;
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     private SerialInputOutputManager serialIOManager;
 
-    private final SerialInputOutputManager.Listener mListener =
+    // queue of received data from the Arduino
+    public Queue<String> retrievedData = new LinkedList<String>();
+
+    private final SerialInputOutputManager.Listener serialListener =
             new SerialInputOutputManager.Listener() {
 
                 @Override
@@ -61,9 +55,11 @@ public class ArduinoController extends Activity {
     }
 
     private void updateReceivedData(byte[] data) {
-        final String message = "Read " + data.length + " bytes: \n"
-                + HexDump.dumpHexString(data) + "\n\n";
-        sendCommandText.setText(message);  // display received data
+        // TODO: parse data first? call another function?
+        retrievedData.add(new String(data));
+//        final String message = "Read " + data.length + " bytes: \n"
+//                + HexDump.dumpHexString(data) + "\n\n";
+//        sendCommandText.setText(message);  // display received data
 //        mDumpTextView.append(message);
 //        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
     }
@@ -79,40 +75,78 @@ public class ArduinoController extends Activity {
     private void startIoManager() {
         if (arduinoPort != null) {
 //            Log.i(TAG, "Starting io manager ..");
-            serialIOManager = new SerialInputOutputManager(arduinoPort, mListener);
+            serialIOManager = new SerialInputOutputManager(arduinoPort, serialListener);
             executor.submit(serialIOManager);
         }
     }
 
 
 
-    public static byte[] stringToBytesASCII(String str) {
-        char[] buffer = str.toCharArray();
-        byte[] b = new byte[buffer.length];
+    private static byte[] stringToBytesASCII(String str) {
+        byte[] b = new byte[str.length()];
         for (int i = 0; i < b.length; i++) {
-            b[i] = (byte) buffer[i];
+            b[i] = (byte) str.charAt(i);
         }
         return b;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: don't have displayable activity?
-        setContentView(R.layout.activity_arduino_controller);
 
-        // setup text
-        sendCommandText = (TextView) findViewById(R.id.sendCommandText);
-        statusText = (TextView) findViewById(R.id.statusText);
-
-        // Find all available drivers from attached devices
-        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
+    public static int write(String data){
+        if (arduinoPort == null)
+            return 0;
+        int bytesWritten;
+        try {
+            bytesWritten = arduinoPort.write(stringToBytesASCII(data), 1000);
+        } catch (IOException e) {
+            return 0;
+        }
+        return bytesWritten;
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+    public static String start(Context context){
+        // Find all available drivers from attached devices
+        if (usbManager == null)
+            usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+
+        SystemClock.sleep(1000);
+        List<UsbSerialDriver> drivers =
+                UsbSerialProber.getDefaultProber().findAllDrivers(usbManager);
+        if (drivers.isEmpty()){
+            return "No drivers found!";
+        }
+        // TODO: for now we are assuming the first one is the arduino....
+        arduinoPort = drivers.get(0).getPorts().get(0);
+
+        if (arduinoPort == null)
+            return "Arduino Not Found!";
+
+
+        UsbDeviceConnection connection = usbManager.openDevice(arduinoPort.getDriver().getDevice());
+        if (connection == null){
+            return "Opening device failed!";
+        }
+
+        try {
+            arduinoPort.open(connection);
+            // set to send over 8 bits
+            arduinoPort.setParameters(115200, UsbSerialPort.DATABITS_8,
+                    UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            arduinoPort.close();
+        } catch (IOException e) {
+            String result = "Error opening device: " + e.getMessage();
+            try {
+                arduinoPort.close();
+            } catch (IOException e2){
+                // Ignore
+            }
+            return result;
+        }
+
+        onDeviceStateChange();  // Restart the IO manager
+        return "Connected to: " + arduinoPort.getClass().getSimpleName();
+    }
+
+    public static void pause() {
         if (arduinoPort != null) {
             try {
                 arduinoPort.close();
@@ -121,88 +155,6 @@ public class ArduinoController extends Activity {
             }
             arduinoPort = null;
         }
-        finish();
     }
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-
-        SystemClock.sleep(1000);
-        List<UsbSerialDriver> drivers =
-                UsbSerialProber.getDefaultProber().findAllDrivers(usbManager);
-        if (drivers.isEmpty()){
-            statusText.setText("No drivers found!");
-            return;
-        }
-        // TODO: for now we are assuming the first one is the arduino....
-        arduinoPort = drivers.get(0).getPorts().get(0);
-
-        if (arduinoPort == null){
-            statusText.setText("Arduino Not Found!");
-        } else {
-
-            UsbDeviceConnection connection = usbManager.openDevice(arduinoPort.getDriver().getDevice());
-            if (connection == null){
-                statusText.setText("Opening device failed!");
-                return;
-            }
-
-//            // Open a connection to the first available driver.
-//            UsbSerialDriver driver = availableDrivers.get(0);
-//            UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-//            if (connection == null) {
-//                statusText.setText("Opening device Failed!");
-//                return;
-//            }
-//
-//            // TODO: create helper functions for this
-//            // Write some data..
-//            arduinoPort = driver.getPorts().get(0);
-            try {
-                arduinoPort.open(connection);
-                // set to send over 8 bits
-                arduinoPort.setParameters(115200, UsbSerialPort.DATABITS_8,
-                        UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-
-                // Send command to go forward then backward  for testing purposes
-                arduinoPort.write(stringToBytesASCII("F"), 1000);
-//                Thread.sleep(5000);
-                arduinoPort.write(stringToBytesASCII("B"), 1000);
-//                Thread.sleep(5000);
-
-                arduinoPort.close();
-            } catch (IOException e) {
-                statusText.setText("Error opening device: " + e.getMessage());
-                try {
-                    arduinoPort.close();
-                } catch (IOException e2){
-                    // Ignore
-                }
-                return;
-            }
-            statusText.setText("Connected to: " + arduinoPort.getClass().getSimpleName());
-        }
-        onDeviceStateChange();  // Restart the IO manager
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.arduino_controller, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ Robo/app/src/main/java/edu/umd/enee408i/robo/Main.java
@@ -0,0 +1,57 @@
+package edu.umd.enee408i.robo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+//import org.opencv.core;
+
+
+public class Main extends Activity {
+
+    TextView statusText;
+    TextView sendCommandText;  // Center text that displays command being sent to arduino
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: don't have displayable activity?
+        setContentView(R.layout.activity_main);
+
+        // setup text
+        sendCommandText = (TextView) findViewById(R.id.sendCommandText);
+        statusText = (TextView) findViewById(R.id.statusText);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ArduinoController.pause();
+        finish();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        ArduinoController.start(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.arduino_controller, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+}
