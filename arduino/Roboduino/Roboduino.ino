3677b560537515c3bfb4e36697318859b9e821bb
jpsnyder <jonsnyder1@gmail.com>
2014-11-05 11:22:23 -0500

    add interface for arduino usb connection

--- arduino/Roboduino/Roboduino.ino
+++ arduino/Roboduino/Roboduino.ino
@@ -1,14 +1,23 @@
 #include <Servo.h>
-
+#include <stdlib.h>
+#define LEFT 0
+#define RIGHT 1
+#define THRESHOLD 30   // Threshold in inches
+#define HIGH_SPEED 150
+#define LOW_SPEED 80
+#define STOP 0
+#define LEFT_OFFSET 6  // extra speed to compensate for left motor going slower
+#define ONE_ROTATION 3200  // number of ticks per rotation
+#define PI 3.14159265359
 
 //left wheel encoder
 const int encoderLPinA = 2;
 const int encoderLPinB = 3;
-volatile unsigned int encoderLPos;
+volatile unsigned long encoderLPos;
 // right wheel encoder
 const int encoderRPinA = 4;
 const int encoderRPinB = 5;
-volatile unsigned int encoderRPos;
+volatile unsigned long encoderRPos;
 // right wheel motor
 const int INA1 = 8;
 const int INB1 = 9;
@@ -25,7 +34,7 @@ const int ping_left = 24;
 const int ping_servo_left = 7;  // PWM
 
 
-void setup(){
+void setup() {
   //left encoder setup
   pinMode(encoderLPinA, INPUT);
   pinMode(encoderLPinB, INPUT);
@@ -38,7 +47,7 @@ void setup(){
   attachInterrupt(encoderRPinA, doEncoderRA, CHANGE);
   attachInterrupt(encoderRPinB, doEncoderRB, CHANGE);
   encoderRPos = 0;
-  
+
   // set up wheel motors
   pinMode(INA1, OUTPUT);
   pinMode(INB1, OUTPUT);
@@ -47,85 +56,148 @@ void setup(){
   pinMode(INB2, OUTPUT);
   pinMode(PWM2, OUTPUT);
   // debuging serial connection
-  Serial.begin(9600);
+  SerialUSB.begin(152000);
 }
 
-void loop(){
-  // detect pings
-  
-//  move_wheel(true, 64);
-//  move_wheel(false, 64);
-//  
-//  delay(2000);
-//  
-//  move_wheel(true, -64);
-//  move_wheel(false, -64);
-//  
-//  delay(2000);
+void loop() {
+  //  move_robot(LOW_SPEED, 1);
+  //  delay(1000);
+  char msg[6];
+  if (SerialUSB.readBytes(msg, 1 + sizeof(int))) {
+    //msg[5] = '/0';
+    switch ((char) msg[0]) {
+      case 'D':
+        SerialUSB.println(atoi((const char*) (msg + 1)));
+        //Serial.println((int)*(msg+1)-'0');
+        move_robot(LOW_SPEED, atoi((const char*)(msg + 1)));
+        //move_robot(LOW_SPEED, ((int) *(msg+1)-'0'));
+        delay(50);
+        break;
+      case 'R':
+        rotate_robot(LOW_SPEED, atoi((const char*)(msg + 1)));
+        delay(50);
+        break;
+    }
+  }
+  //  move_robot(LOW_SPEED, 0.5);
+  //  rotate_robot(LOW_SPEED, 90);
+  //  move_robot(LOW_SPEED, 8);
+  //  rotate_robot(LOW_SPEED, -90);
+  //  move_robot(LOW_SPEED, 3);
 
-  #define THRESHOLD 30   // Threshold in inches
-  #define HIGH_SPEED 150
-  #define LOW_SPEED 80
-  #define STOP 0
-  #define LEFT_OFFSET 6  // extra speed to compensate for left motor going slower
-  #define ONE_ROTATION 3200  // number of ticks per rotation (offset from 3200)
-  
-//  long left_inches = ping_inches(ping_left, /*THRESHOLD + 5*/0);
-//  long right_inches = ping_inches(ping_right, /*THRESHOLD + 5*/0);
-//
-//  
-//  
-//  int left_speed = ((right_inches < THRESHOLD) ? STOP : HIGH_SPEED) + LEFT_OFFSET;
-//  int right_speed = (left_inches < THRESHOLD) ? STOP : HIGH_SPEED;
-//  
-//  // don't run into a wall...
-//  if (left_speed == STOP && right_speed == STOP){
-//    left_speed = -(LOW_SPEED);
-//    right_speed = -(LOW_SPEED);
-//  }
-
-  Serial.print("L: ");
-  Serial.print(encoderLPos);
-  Serial.print(" R: ");
-  Serial.print(encoderRPos);
-  Serial.println(); 
-
-//  int right_rotations = encoderRPos/ONE_ROTATION;
-//  int left_rotations = encoderLPos/ONE_ROTATION;
-//
-//  
-//  int right_speed = (right_rotations < 3) ? LOW_SPEED : STOP;
-//  int left_speed = (left_rotations < 3) ? LOW_SPEED : STOP;
-//  
-//  move_wheel(true, right_speed);
-//  move_wheel(false, left_speed);
-  
-  // read pings
-//  Serial.print("Left: ");
-//  Serial.print(ping_inches(ping_left, 0));
-//  Serial.print(", Right: ");
-//  Serial.print(ping_inches(ping_right, 0));
-//  Serial.println();
-  
 }
 
-void move_wheel(boolean right_wheel, int wheel_speed){
+//Serial.print("L: ");
+//Serial.print(encoderLPos);
+//Serial.print(" R: ");
+//Serial.print(encoderRPos);
+//Serial.println();
+
+int report_distance(unsigned long distance) {
+  if (SerialUSB.print("D")) {
+    SerialUSB.print(distance);
+  }
+}
+
+int report_angle(unsigned long angle) {
+  if (SerialUSB.print("R")) {
+    SerialUSB.print(angle);
+  }
+}
+
+int follow_instructions() {
+  // read instructions
+  byte buff[50];
+  int size_instructions = sizeof(char) + sizeof(unsigned long);
+  if (SerialUSB.readBytes(buff, size_instructions) == 0)
+    return 0;
+  if ((char) buff[0] == 'D')
+    move_robot(LOW_SPEED, *((unsigned long*) (buff + 1)));
+  if ((char) buff[0] == 'A')
+    rotate_robot(LOW_SPEED, *((unsigned long*) (buff + 1)));
+}
+
+int send_to_android(char *string) {
+  return SerialUSB.write(string);
+}
+
+void rotate_robot(int wheel_speed, float angle) {
+  //counter-clockwise is positive
+  int wheel_speedL = angle > 0 ? -wheel_speed - LEFT_OFFSET : wheel_speed + LEFT_OFFSET;
+  int wheel_speedR = angle > 0 ? wheel_speed : -wheel_speed;
+
+  long target = 15 * abs(angle); // 3600 * (arclength / wheel circumference)
+  encoderLPos = 0;
+  encoderRPos = 0;
+  while (wheel_speedL != STOP || wheel_speedR != STOP) {
+    if (encoderLPos >= target)
+      wheel_speedL = STOP;
+    if (encoderRPos >= target)
+      wheel_speedR = STOP;
+    move_wheel(LEFT, wheel_speedL);
+    move_wheel(RIGHT, wheel_speedR);
+  }
+}
+
+unsigned long move_robot(int wheel_speed, float rotations) {
+  int wheel_speedL = wheel_speed + LEFT_OFFSET;
+  int wheel_speedR = wheel_speed;
+  encoderLPos = 0;
+  encoderRPos = 0;
+  int thresh = 2;
+  //unsigned long start_posL = encoderLPos;
+  //unsigned long start_posR = encoderRPos;
+  //unsigned long start_time = millis();
+  float speedL, speedR;
+
+  while (((encoderLPos / ONE_ROTATION) <= rotations) && ((encoderRPos / ONE_ROTATION) <= rotations)) {
+    if (encoderLPos / ONE_ROTATION <= rotations)
+      move_wheel(LEFT, wheel_speedL);
+    else move_wheel(LEFT, STOP);
+    if (encoderRPos / ONE_ROTATION <= rotations)
+      move_wheel(RIGHT, wheel_speedR);
+    else move_wheel(RIGHT, STOP);
+    long startL = encoderLPos;
+    long startR = encoderRPos;
+    delay(50);
+    //time = millis();
+    long diff = (long)((encoderLPos - startL) - (encoderRPos - startR));
+    //    Serial.print(" Dif: ");
+    //    Serial.println(diff);
+    if (abs(diff) < thresh)
+      continue;
+    else if (diff > 0) {
+      wheel_speedL -= 1;
+      //wheel_speedR += 1;
+    }
+    else {
+      wheel_speedL += 1;
+      //wheel_speedR -= 1;
+    }
+  }
+  move_wheel(RIGHT, STOP);
+  move_wheel(LEFT, STOP);
+  return encoderLPos;  // return rough distance it went
+}
+
+void move_wheel(boolean right_wheel, int wheel_speed) {
   // moves the given wheel at given direction at given speed (25% = 64; 50% = 127; 75% = 191; 100% = 255)
   // is non-blocking
   int first = 0;
   int second = 0;
   int pins[3];
-  
-  if (right_wheel){
+
+  // Set INA, INB
+  if (right_wheel) {
     pins[0] = INA1;
     pins[1] = INB1;
     pins[2] = PWM1;
-    if (wheel_speed > 0){
+    if (wheel_speed > 0) {
       first = LOW;
       second = HIGH;
-    } else if (wheel_speed < 0){
+    } else if (wheel_speed < 0) {
       first = HIGH;
-      second = LOW; 
+      second = LOW;
     } else {
       first = LOW;
       second = LOW;
@@ -134,30 +206,31 @@ void move_wheel(boolean right_wheel, int wheel_speed){
     pins[0] = INA2;
     pins[1] = INB2;
     pins[2] = PWM2;
-    if (wheel_speed > 0){
+    if (wheel_speed > 0) {
       first = HIGH;
       second = LOW;
-    } else if (wheel_speed < 0){
+    } else if (wheel_speed < 0) {
       first = LOW;
-      second = HIGH; 
+      second = HIGH;
     } else {
       first = LOW;
-      second = LOW; 
+      second = LOW;
     }
   }
-  
+
+
   // TODO: offset one of the motors
   digitalWrite(pins[0], first);
   digitalWrite(pins[1], second);
   analogWrite(pins[2], abs(wheel_speed));
 }
 
-long inches_to_microseconds(long inches){
+long inches_to_microseconds(long inches) {
   return inches * 2 * 74;
 }
 
-long centimeters_to_microseconds(long centimeters){
-  return centimeters * 2 * 29; 
+long centimeters_to_microseconds(long centimeters) {
+  return centimeters * 2 * 29;
 }
 
 long microseconds_to_inches(long microseconds)
@@ -179,9 +252,9 @@ long microseconds_to_centimeters(long microseconds)
 }
 
 // TODO: make this non-blocking
-long ping_duration(int ping_pin, unsigned long timeout){
+long ping_duration(int ping_pin, unsigned long timeout) {
   // returns the duration it took to receive a pong after a ping
-  
+
   // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   pinMode(ping_pin, OUTPUT);
@@ -195,30 +268,33 @@ long ping_duration(int ping_pin, unsigned long timeout){
   // pulse whose duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   pinMode(ping_pin, INPUT);
-  
+
   int duration;
-  if (timeout){
-      duration = pulseIn(ping_pin, HIGH, timeout);
+  if (timeout) {
+    duration = pulseIn(ping_pin, HIGH, timeout);
   } else {
-      duration = pulseIn(ping_pin, HIGH);
+    duration = pulseIn(ping_pin, HIGH);
   }
   return duration;
 }
 
-long ping_inches(int ping_pin, unsigned long timeout){
+long ping_inches(int ping_pin, unsigned long timeout) {
   return microseconds_to_inches(ping_duration(ping_pin, inches_to_microseconds(timeout)));
 }
 
-long ping_centimeters(int ping_pin, unsigned long timeout){
+long ping_centimeters(int ping_pin, unsigned long timeout) {
   return microseconds_to_centimeters(ping_duration(ping_pin, centimeters_to_microseconds(timeout)));
 }
+
+
+// Wheel Encoder Interrupt Callbacks ========
 
 void doEncoderLA() {
   encoderLPos++;
 }
 
 void doEncoderLB() {
-  encoderLPos--;  
+  encoderLPos++;
 }
 
 void doEncoderRA() {
@@ -226,5 +302,27 @@ void doEncoderRA() {
 }
 
 void doEncoderRB() {
-  encoderRPos--;  
+  encoderRPos++;
 }
+
+
+//  long left_inches = ping_inches(ping_left, /*THRESHOLD + 5*/0);
+//  long right_inches = ping_inches(ping_right, /*THRESHOLD + 5*/0);
+//
+//
+//
+//  int left_speed = ((right_inches < THRESHOLD) ? STOP : HIGH_SPEED) + LEFT_OFFSET;
+//  int right_speed = (left_inches < THRESHOLD) ? STOP : HIGH_SPEED;
+//
+//  // don't run into a wall...
+//  if (left_speed == STOP && right_speed == STOP){
+//    left_speed = -(LOW_SPEED);
+//    right_speed = -(LOW_SPEED);
+//  }
+// read pings
+//  Serial.print("Left: ");
+//  Serial.print(ping_inches(ping_left, 0));
+//  Serial.print(", Right: ");
+//  Serial.print(ping_inches(ping_right, 0));
+//  Serial.println();
+
