95945dd361ddbfd8c4ce415e8bc4898bd0ac536f
jpsnyder <jonsnyder1@gmail.com>
2014-12-14 18:31:36 -0500

    clean up code and make comments (arduino code)

--- arduino/Roboduino/Roboduino.ino
+++ arduino/Roboduino/Roboduino.ino
@@ -65,14 +65,11 @@ void setup() {
   pinMode(INA2, OUTPUT);
   pinMode(INB2, OUTPUT);
   pinMode(PWM2, OUTPUT);
-  // debuging serial connection
   Serial.begin(9600);
 }
 
 void loop() {
 
-  //wall_follow(120, RIGHT);
-  //wall_follow(120, LEFT);
   // follow instructions given by arduino
   char msg[6];
   if (Serial.readBytes(msg, 1 + sizeof(int))) {
@@ -95,8 +92,8 @@ void loop() {
   
 }
 
+// rotates the robot counter-clockwise the given angle
 void rotate_robot(int wheel_speed, float angle) {
-  //counter-clockwise is positive
   int wheel_speedL = angle > 0 ? -wheel_speed - LEFT_OFFSET : wheel_speed + LEFT_OFFSET;
   int wheel_speedR = angle > 0 ? wheel_speed : -wheel_speed;
 
@@ -123,15 +120,14 @@ void rotate_robot(int wheel_speed, float angle) {
   Serial.write('A');  // send acknowledgement
 }
 
+// moves the robot forward at the given number of rotations of the wheel
 void move_robot(int wheel_speed, float rotations) {
   int wheel_speedL = wheel_speed + LEFT_OFFSET;
   int wheel_speedR = wheel_speed;
   encoderLPos = 0; // reset wheel encoders
   encoderRPos = 0;
   int thresh = 2;
-  //unsigned long start_posL = encoderLPos;
-  //unsigned long start_posR = encoderRPos;
-  //unsigned long start_time = millis();
+]
   float speedL, speedR;
   long left_inches = 17, right_inches = 17;
   while ((((encoderLPos / ONE_ROTATION) <= rotations) && ((encoderRPos / ONE_ROTATION) <= rotations))) {
@@ -144,19 +140,15 @@ void move_robot(int wheel_speed, float rotations) {
     long startL = encoderLPos;
     long startR = encoderRPos;
     delay(50);
-    //time = millis();
     long diff = (long)((encoderLPos - startL) - (encoderRPos - startR));
-    //    Serial.print(" Dif: ");
-    //    Serial.println(diff);
+    
     if (abs(diff) < thresh)
       continue;
     else if (diff > 0) {
       wheel_speedL -= 1;
-      //wheel_speedR += 1;
     }
     else {
       wheel_speedL += 1;
-      //wheel_speedR -= 1;
     }
 
     // determine if android called stop or ping sensors detected wall too close
@@ -175,9 +167,12 @@ void move_robot(int wheel_speed, float rotations) {
   Serial.write('A');  // send acknowledgement
 }
 
+// sets the given wheel to the given wheel speed
+// (25% = 64; 50% = 127; 75% = 191; 100% = 255)
+// negative numbers make the wheel go backwards
+// 0 stops the wheel
+// is non-blocking
 void move_wheel(boolean right_wheel, int wheel_speed) {
-  // moves the given wheel at given direction at given speed (25% = 64; 50% = 127; 75% = 191; 100% = 255)
-  // is non-blocking
   int first = 0;
   int second = 0;
   int pins[3];
@@ -214,7 +209,6 @@ void move_wheel(boolean right_wheel, int wheel_speed) {
   }
 
 
-  // TODO: offset one of the motors
   digitalWrite(pins[0], first);
   digitalWrite(pins[1], second);
   analogWrite(pins[2], abs(wheel_speed));
@@ -246,7 +240,6 @@ long microseconds_to_centimeters(long microseconds)
   return microseconds / 29 / 2;
 }
 
-// TODO: make this non-blocking
 long ping_duration(int ping_pin, unsigned long timeout) {
   // returns the duration it took to receive a pong after a ping
 
@@ -273,6 +266,8 @@ long ping_duration(int ping_pin, unsigned long timeout) {
   return duration;
 }
 
+// sets the robot to follow the wall at the given speed with the wall on either the left or right
+// uses a PID controller
 void wall_follow(int spd, int dir) {
 
   char msg;
@@ -295,7 +290,6 @@ void wall_follow(int spd, int dir) {
   long max_wall_thresh = 50;
   long wall_dist, check_dist, prev_wall_dist;
   while (1) {
-    //float kp = 0.6, ki = 0.05, kd = 0.1;
     float kp = 0.7, ki = 0.05, kd = 0.2, output, dt;
     float previous_error = 0;
     float integral = 0;
@@ -326,23 +320,7 @@ void wall_follow(int spd, int dir) {
         move_wheel(LEFT, spd - output + LEFT_OFFSET);
         move_wheel(RIGHT, spd + output);
       }
-      
 
-
-
-
-      /*
-      Serial.print("Output = ");
-      Serial.println(output);
-          Serial.print("Integral = ");
-      Serial.println(integral);
-          Serial.print("derivative = ");
-      Serial.println(derivative);
-              Serial.print("error = ");
-      Serial.println(error);
-      */
-      //Serial.print("wall_dist = ");
-      //Serial.println(wall_dist);
     } // end wall following adventure
     
     move_wheel(LEFT, STOP);
@@ -356,105 +334,6 @@ void wall_follow(int spd, int dir) {
     rotate_robot(LOW_SPEED, (dir == LEFT) ? -90 : 90);
   }
 }
-
-
-
-
-
-
-
-
-
-
-/*
-while ( 1 ) {
-  // align servos
-  servo_right.write(servo_right_angle);
-  servo_left.write(servo_left_angle);
-
-  // check ping distances
-  wall_dist = ping_inches(wall_ping, 0);
-  check_dist = ping_inches(check_ping, 0);
-
-  // robot is too far away from the wall, turn towards the wall.
-  if ( max_wall_thresh > wall_dist > (wall_thresh + margin)) {
-    // wall is right next to the robot, continue moving straight
-  } else if ( wall_dist > max_wall_thresh) {
-    // wall has disapear, carve the corner
-    if (dir == LEFT){
-      left_speed += 5;
-      right_speed = 0;
-
-    } else {
-      left_speed = 0;
-      right_speed += 5;
-    }
-
-  }
-
-//      if (dir == LEFT) {
-//        left_speed -= 5;
-//        right_speed += 5;
-//      } else {
-//        left_speed += 5;
-//        right_speed -= 5;
-//      }
-  } else if ( wall_dist < (wall_thresh - margin)) {
-    if (dir == LEFT) {
-      left_speed += 5;
-      right_speed = 0;
-    } else {
-      left_speed = 0;
-      right_speed += 5;
-    }
-  }
-
-
-  // determine if a corner
-  // if check_dist < 10 turn oposite of wall else if wall_dist > big threshold, rotate towards the wall
-
-//    if (check_dist < 10) {
-//      if (dir == LEFT){
-//        // rotate 90 degrees clock-wise
-//        rotate_robot(LOW_SPEED, 90);
-//      } else {
-//        // rotate 90 degrees counter clock-wise
-//        rotate_robot(LOW_SPEED, -90);
-//      }
-//      // reset speeds
-//      left_speed = spd + LEFT_OFFSET;
-//      right_speed = spd;
-//    // check if wall_ping is very far from the wall, we hit a corner
-//    } else if (wall_ping > 24){
-//      // clear away from the the wall
-//      move_robot(LOW_SPEED, 2);
-//      // rotate towards the previously existing wall
-//      if (dir == LEFT){
-//        rotate_robot(LOW_SPEED, -90);
-//      } else {
-//        rotate_robot(LOW_SPEED, 90);
-//      }
-//      // set up position
-//      move_robot(LOW_SPEED, 2);
-//      left_speed = spd + LEFT_OFFSET;
-//      right_speed = spd;
-//    }
-
-
-  move_wheel(LEFT, left_speed);
-  move_wheel(RIGHT, right_speed);
-
-  if ((Serial.readBytes(&msg, 1) && msg == 'S') || left_speed == STOP || right_speed == STOP) {
-    servo_right.write(130);
-    servo_left.write(30);
-    move_wheel(LEFT, STOP);
-    move_wheel(RIGHT, STOP);
-    Serial.write('S'); // notify we stopped instead of completed
-    return;
-  }
-}
-}
-*/
 
 
 long ping_inches(int ping_pin, unsigned long timeout) {
@@ -483,25 +362,4 @@ void doEncoderRA() {
 void doEncoderRB() {
   encoderRPos++;
 }
-
-
-//  long left_inches = ping_inches(ping_left, /*THRESHOLD + 5*/0);
-//  long right_inches = ping_inches(ping_right, /*THRESHOLD + 5*/0);
-//
-//
-//
-//  int left_speed = ((right_inches < THRESHOLD) ? STOP : HIGH_SPEED) + LEFT_OFFSET;
-//  int right_speed = (left_inches < THRESHOLD) ? STOP : HIGH_SPEED;
-//
-//  // don't run into a wall...
-//  if (left_speed == STOP && right_speed == STOP){
-//    left_speed = -(LOW_SPEED);
-//    right_speed = -(LOW_SPEED);
-//  }
-// read pings
-//  Serial.print("Left: ");
-//  Serial.print(ping_inches(ping_left, 0));
-//  Serial.print(", Right: ");
-//  Serial.print(ping_inches(ping_right, 0));
-//  Serial.println();
 
